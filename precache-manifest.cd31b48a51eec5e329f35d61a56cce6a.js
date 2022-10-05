59f4252fd13d96d6a6909eac693d616e5cecff9e
michelheredia <herediamichel18@gmail.com>
2022-10-05 13:46:41 -0300

    Updates

---
+++ precache-manifest.cd31b48a51eec5e329f35d61a56cce6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7865f0a348b958d44d89",
+    "url": "/ta-te-ti/static/css/main.0bd6fe78.chunk.css"
+  },
+  {
+    "revision": "7865f0a348b958d44d89",
+    "url": "/ta-te-ti/static/js/main.7865f0a3.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/ta-te-ti/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "6d79483b4d70c3326bf4",
+    "url": "/ta-te-ti/static/js/2.6d79483b.chunk.js"
+  },
+  {
+    "revision": "1bd6c4a1f4dfafb86ae2",
+    "url": "/ta-te-ti/static/js/runtime~main.1bd6c4a1.js"
+  },
+  {
+    "revision": "e96e2f37a640ce64f41aef0ba4ab1c8b",
+    "url": "/ta-te-ti/index.html"
+  }
+];
